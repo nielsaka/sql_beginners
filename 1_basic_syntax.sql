a90dbb584b5c02e5e72b76f10f7deeab3d2dafb7
Niels Aka <6137753+nielsaka@users.noreply.github.com>
2018-06-28 02:15:07 +0200

    SELECT col FROM table

---
+++ 1_basic_syntax.sql
@@ -0,0 +1,19 @@
+﻿
+SELECT * FROM actor;
+
+--// # a comment?
+-- comments start with two dashs
+
+/* multi-line comments are as in C-style languages
+for example C or C++. They start with slash star and end with
+*/
+
+SELECT first_name, last_name FROM actor;
+
+SELECT * FROM customer;
+
+-- commands can spread across several lines
+-- white space does not matter (?)
+-- convention: place every keyword on its own line
+SELECT first_name, last_name, email 
+FROM customer;
