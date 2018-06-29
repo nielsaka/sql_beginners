791ce86ae82429896b3143cb190f084eedea6631
Niels Aka <6137753+nielsaka@users.noreply.github.com>
2018-06-29 23:55:09 +0200

    SELECT DISTINCT

--- 1_basic_syntax.sql
+++ 1_basic_syntax.sql
@@ -17,3 +17,19 @@ SELECT * FROM customer;
 -- convention: place every keyword on its own line
 SELECT first_name, last_name, email 
 FROM customer;
+
+
+---------------------------------------------------------
+-- SELECT DISTINCT: select distinct/unique values in colum
+---------------------------------------------------------
+
+SELECT * FROM film;
+
+-- 2006 is the only release year
+SELECT DISTINCT release_year FROM film;
+
+-- three rental rates: 0.99, 2.99 or 4.99 $
+SELECT DISTINCT rental_rate FROM film;
+
+-- all films were updated on 2013-05-26
+SELECT DISTINCT last_update FROM film;
